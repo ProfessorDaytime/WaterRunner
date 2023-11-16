ce2b0106baa0790abdb2d923e275f447f9abc036
Seth <thesx88@gmail.com>
2023-11-15 16:13:42 -0800

    Got basics of Climbing working
    Climbing with animation works in a new scene.  Still gotta combine it with the original character controller.

---
+++ Assets/Scripts/PlayerIK.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIK : MonoBehaviour
+{
+
+    ClimbController3 movementScript;
+    Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = transform.GetComponent<Animator>();
+        movementScript = transform.parent.GetComponent<ClimbController3>();
+    }
+
+    void OnAnimatorIK(int layerIndex){
+        print("ANIMATOR IK");
+        
+        if(movementScript.state == ClimbController3.PlayerState.CLIMBING){
+            HumanBodyBones[] ikBones = {
+                HumanBodyBones.LeftFoot,
+                HumanBodyBones.RightFoot,
+                HumanBodyBones.LeftHand,
+                HumanBodyBones.RightHand
+            };
+
+            for(int i = 0; i < ikBones.Length; i++){
+                AvatarIKGoal goal = (AvatarIKGoal)i;
+                Transform t = animator.GetBoneTransform(ikBones[i]);
+                Vector3 direction = transform.parent.forward;
+
+                RaycastHit boneHit;
+                if(Physics.Raycast(t.position - direction, direction * 3f, out boneHit)){
+                    animator.SetIKPosition(goal, boneHit.point - direction * 0.1f);
+                    animator.SetIKPositionWeight(goal, 1f);
+                } else{
+                    animator.SetIKPositionWeight(goal, 0f);
+                }
+            }
+        } else{
+            for(int i = 0; i < 4; i++){
+                animator.SetIKPositionWeight((AvatarIKGoal)i, 0f);
+            }
+        }
+    }
+}
